14b80522fa853be4aca9dd69631a15443d3fdbc3
nikhilaryan92 <38068424+nikhilaryan92@users.noreply.github.com>
2019-07-01 17:44:52 +0530

    Update ttest.py

--- ttest.py
+++ ttest.py
@@ -1,6 +1,6 @@
 from scipy import stats
 import numpy
-dataset_clinical = numpy.loadtxt("/home/nikhil/Desktop/Project/nik/T test/ttest.csv", delimiter=" ")
+dataset_clinical = numpy.loadtxt("/home/nikhil/Desktop/Project/nik/T test/ttest_metbric.csv", delimiter=" ")
 a = dataset_clinical[:,0:1]
 b = dataset_clinical[:,1]
 
